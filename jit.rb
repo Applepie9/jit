e65d6b35f3b57605eac2ac32c78e16876b4e6afe
Applepie9 <horel.dora@gmail.com>
2024-03-20 10:42:17 +0000

    init, read path and handle errors

---
+++ jit.rb
@@ -0,0 +1,28 @@
+require "fileutils"
+require "pathname"
+
+command = ARGV.shift
+
+case command
+when "init"
+  path = ARGV.fetch(0, Dir.getwd)
+  
+  root_path = Pathname.new(File.expand_path(path))
+  git_path = root_path.join(".git")
+  
+  ["objects", "refs"].each do |dir|
+    begin
+      FileUtils.mkdir_p(git_path.join(dir))
+    rescue Errno::EACCES => error
+      $stderr.puts "fatal: #{ error.message }"
+      exit 1
+    end
+  end
+
+  puts "Initialized empty Jit repository in #{ git_path }"
+  exit 0
+
+else
+  $stderr.puts "jit: '#{ command }' is not a jit command."
+  exit 1
+end
