15f6f89f3d4727d8dc95490788e528644f03b0a8
Applepie9 <horel.dora@gmail.com>
2024-04-10 14:12:48 +0100

    DRY code, Extract commands to classes

---
+++ lib/command/init.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal: true
+
+require "fileutils"
+require "pathname"
+
+module Command
+  class Init
+    def run
+      path = ARGV.fetch(0, Dir.getwd)
+
+      root_path = Pathname.new(File.expand_path(path))
+      git_path  = root_path.join(".git")
+
+      %w[objects refs].each do |dir|
+        FileUtils.mkdir_p(git_path.join(dir))
+      rescue Errno::EACCES => e
+        warn "fatal: #{e.message}"
+        exit 1
+      end
+
+      puts "Initialized empty Jit repository in #{git_path}"
+      exit 0
+    end
+  end
+end
