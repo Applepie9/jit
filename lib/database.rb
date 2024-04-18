de3198f8335eb6989881a76cfff7b0bc69f7c7e5
Applepie9 <horel.dora@gmail.com>
2024-04-18 16:44:26 +0100

    Add ability to read object

--- lib/database.rb
+++ lib/database.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 require "digest/sha1"
+require "strscan"
 require "zlib"
 
 require_relative "database/author"
@@ -11,8 +12,15 @@ require_relative "database/tree"
 class Database
   TEMP_CHARS = ("a".."z").to_a + ("A".."Z").to_a + ("0".."9").to_a
 
+  TYPES = {
+    "blob" => Blob,
+    "tree" => Tree,
+    "commit" => Commit
+  }.freeze
+
   def initialize(pathname)
     @pathname = pathname
+    @objects = {}
   end
 
   def store(object)
@@ -23,6 +31,10 @@ class Database
 
   def hash_object(object)
     hash_content(serialize_object(object))
+  end
+
+  def load(oid)
+    @objects[oid] ||= read_object(oid)
   end
 
   private
@@ -36,11 +48,28 @@ class Database
     Digest::SHA1.hexdigest(string)
   end
 
-  def write_object(oid, content)
-    object_path = @pathname.join(oid[0..1], oid[2..])
-    return if File.exist?(object_path)
+  def object_path(oid)
+    @pathname.join(oid[0..1], oid[2..])
+  end
 
-    dirname = object_path.dirname
+  def read_object(oid)
+    data    = Zlib::Inflate.inflate(File.read(object_path(oid)))
+    scanner = StringScanner.new(data)
+
+    type  = scanner.scan_until(/ /).strip
+    _size = scanner.scan_until(/\0/)[0..-2]
+
+    object = TYPES[type].parse(scanner)
+    object.oid = oid
+
+    object
+  end
+
+  def write_object(oid, content)
+    path = object_path(oid)
+    return if File.exist?(path)
+
+    dirname   = path.dirname
     temp_path = dirname.join(generate_temp_name)
 
     begin
@@ -55,7 +84,7 @@ class Database
     file.write(compressed)
     file.close
 
-    File.rename(temp_path, object_path)
+    File.rename(temp_path, path)
   end
 
   def generate_temp_name
