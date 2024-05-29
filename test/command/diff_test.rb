003d6ca314323613bd133655eaaa807e2f91abe7
Applepie9 <horel.dora@gmail.com>
2024-05-29 15:18:44 +0100

    Diff Command - Display diff headers for files deleted from the workspace

--- test/command/diff_test.rb
+++ test/command/diff_test.rb
@@ -58,5 +58,17 @@ describe Command::Diff do
         +++ b/file.txt
       DIFF
     end
+
+    it "diffs a deleted file" do
+      delete "file.txt"
+
+      assert_diff <<~DIFF
+        diff --git a/file.txt b/file.txt
+        deleted file mode 100644
+        index 12f00e9..0000000
+        --- a/file.txt
+        +++ /dev/null
+      DIFF
+    end
   end
 end
